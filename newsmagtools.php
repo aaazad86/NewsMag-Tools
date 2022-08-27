632adaae4adc5d1f0281402acd10a89d0dee06b2
Al Amin Azad <6736991+aaazad86@users.noreply.github.com>
2022-08-27 16:18:13 +0600

    Update newsmagtools.php

--- newsmagtools.php
+++ newsmagtools.php
@@ -20,3 +20,28 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Update URI:        https://example.com/my-plugin/
  */
+
+
+/**
+ * Activate the plugin.
+ */
+function pluginprefix_activate() { 
+    // Trigger our function that registers the custom post type plugin.
+    pluginprefix_setup_post_type(); 
+    // Clear the permalinks after the post type has been registered.
+    flush_rewrite_rules(); 
+}
+register_activation_hook( __FILE__, 'pluginprefix_activate' );
+
+
+/**
+ * Deactivation hook.
+ */
+function pluginprefix_deactivate() {
+    // Unregister the post type, so the rules are no longer in memory.
+    unregister_post_type( 'book' );
+    // Clear the permalinks to remove our post type's rules from the database.
+    flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'pluginprefix_deactivate' );
+

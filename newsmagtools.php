e9bba6578b33f9771dc90b0fee193d965f663942
Al Amin Azad <6736991+aaazad86@users.noreply.github.com>
2022-08-27 15:55:54 +0600

    Update newsmagtools.php

--- newsmagtools.php
+++ newsmagtools.php
@@ -1,1 +1,22 @@
-
+/**
+ * Plugin Name
+ *
+ * @package           Primary Release @1.0.1
+ * @author            Al Amin Azad
+ * @copyright         2022 Bondhon IT
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       News Magazine Tools
+ * Plugin URI:        https://bondhon.net/plugin-name
+ * Description:       Various tool for anyonline newspaper.
+ * Version:           1.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Al Amin Azad
+ * Author URI:        https://bondhon.net
+ * Text Domain:       News-Mag-Tools
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */

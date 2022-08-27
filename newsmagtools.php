8a84aee1c86e794a36a9cf9483d4b319888e8906
Al Amin Azad <6736991+aaazad86@users.noreply.github.com>
2022-08-27 17:06:40 +0600

    Update newsmagtools.php

--- newsmagtools.php
+++ newsmagtools.php
@@ -27,7 +27,7 @@
  */
 function newsmagtools_activate() { 
     // Trigger our function that registers the custom post type plugin.
-    pluginprefix_setup_post_type(); 
+    newsmagtools_setup_post_type(); 
     // Clear the permalinks after the post type has been registered.
     flush_rewrite_rules(); 
 }
